ea079d4f69a929cc1b6cc9155d9830e1d33e8073
Tanvi-27 <tanvighodke1027.com>
2024-04-22 15:34:02 +0530

    add new loading screen

--- src/Pages/Loading.jsx
+++ src/Pages/Loading.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import LoadingScreen from "../assets/loadingscreen.gif"
+// import LoadingScreen from "../assets/loadingscreen.gif"
+import LoadingScreen from "../assets/loadingBean.gif"
 
 function Loading() {
     return (
