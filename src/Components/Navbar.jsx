c1edefeffa86b089efc1b3664253687041af93c3
Tanvi-27 <tanvighodke1027.com>
2024-04-22 15:47:42 +0530

    deleted unnecessary comments

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,14 +9,6 @@ function Navbar() {
         <div className="nav-bar-logo-container">
             <Link to="/">NomadNest</Link>
         </div>
-        {/* <div className="nav-bar-links-container">
-            <div className="nav-bar-link">
-                <a className="nav-bar-link-login" href="">Log in</a>
-            </div>
-            <div className="nav-bar-link">
-                <a className="nav-bar-link-start" href=''>Start</a>
-            </div>
-        </div> */}
     </div>
     </div>
   )
